d3f1ecc8442cda790b736a9b3e7adeae1e846aa7
btopro <bto108@psu.edu>
2024-10-16 17:11:23 -0400

    https://github.com/haxtheweb/issues/issues/2170

--- src/cli.js
+++ src/cli.js
@@ -4,58 +4,27 @@
 process.env.haxcms_middleware = "node-cli";
 // HAXcms core settings
 const { HAXCMS } = require('./lib/HAXCMS.js');
-/**
- * @todo need a configuration resolver of some kind
- * if we are invoking stand alone, it'll need to install haxcms in place
- * if it's in an existing HAXcms deploy, it should read off that _config / other multi-site directories
- * if it's a HAXSite then it needs to supply config that works relative to that one
- * This also influnces the entry index.html file
- */
 
 const RoutesMap = require('./lib/RoutesMap.js');
 
 // process arguments from commandline appropriately
-const args = process.argv;
-args.shift();
-args.shift();
 let body = {};
-let cliOp = 'listCalls';
-// 1st 2 args call the program itself
-for (var i in args) {
-  let arg = args[i].split('=');
-  let param = arg[0].replace('--','');
-  let value = arg[1];
-  switch (param) {
-    case 'op':
-      cliOp = value;
-    break;
-    case "nodeTitle":
-      body.node = body.node || {};
-      body.node.title = value;
-    break;
-    case "site":
-      body.site = {
-        name: value
-      };
-    break;
-    // set everything else as parameters
-    default:
-      body[param] = value;
-    break;
-  }
-}
+let cliOp = null;
 const cli = {
-  post: (path, callback) => callback({
-    route: {
-      path: path
+  post: (path, callback) => 
+    callback(
+    {
+      route: {
+        path: path
+      },
+      body: body,
+      method: "post"
     },
-    body: body,
-    method: "post"
-  },
-  {
-    query: {},
-    send: (data) => console.log(data),
-  }),
+    {
+      query: {},
+      send: (data) => console.log(data),
+    }
+  ),
   get: (path, callback) => callback({
     route: {
       path: path
@@ -92,3 +61,28 @@ for (var method in RoutesMap) {
     }
   }
 }
+
+// method to bridge api calls in similar manner given a site already loaded into scope
+export function cliBridge(op, body = {}) {
+  let req = {
+    route: {
+      path: `${HAXCMS.basePath}${HAXCMS.systemRequestBase}${route}`
+    },
+    body: body,
+    method: "post"
+  };
+  let res = {
+    query: {},
+    send: (data) => console.log(data),
+  };
+  const rMethod = req.method.toLowerCase();
+  if (HAXCMS.validateJWT(req, res)) {
+    // call the method
+    RoutesMap.RoutesMap[rMethod][op](req, res);
+  }
+  else {
+    console.error("route connection issue");
+  }
+}
+
+export { cli };
