e9773d1996233f9bafb06832b8220ec2a98bab34
btopro <bto108@psu.edu>
2025-07-21 14:01:26 -0400

    https://github.com/haxtheweb/issues/security/advisories/GHSA-pjj3-j5j6-qj27

--- src/routes/listFiles.js
+++ src/routes/listFiles.js
@@ -21,46 +21,49 @@ const mime = require('mime');
    */
   async function listFiles(req, res) {
     let files = [];
-    let site = await HAXCMS.loadSite(req.query['siteName']);
-    if (site && site.siteDirectory) {
-      let search = (typeof req.query['filename'] !== 'undefined') ? req.query['filename'] : '';
-      // build files directory path
-      let siteFilePath = path.join(site.siteDirectory, 'files');
-      let handle;
-      if (handle = fs.readdirSync(siteFilePath)) {
-        handle.forEach(file => {
-          if (
-              file != "." &&
-              file != ".." &&
-              file != '.gitkeep' &&
-              file != '.DS_Store'
-          ) {
-            // ensure this is a file
+    // verify that we have params expected from frontend
+    if (req.query && req.query['siteName']) {
+      let site = await HAXCMS.loadSite(req.query['siteName']);
+      if (site && site.siteDirectory) {
+        let search = (typeof req.query['filename'] !== 'undefined') ? req.query['filename'] : '';
+        // build files directory path
+        let siteFilePath = path.join(site.siteDirectory, 'files');
+        let handle;
+        if (handle = fs.readdirSync(siteFilePath)) {
+          handle.forEach(file => {
             if (
-              fs.lstatSync(siteFilePath + '/' + file).isFile()
+                file != "." &&
+                file != ".." &&
+                file != '.gitkeep' &&
+                file != '.DS_Store'
             ) {
-              // ensure this is a file and if we are searching for results then return only exact ones
-              if (search == "" || file.indexOf(search) !== -1) {
-                let fullUrl = '/files/' + file;
-                // multiple sites then append the base url to site management area
-                if (HAXCMS.operatingContext == 'multisite') {
-                  fullUrl = HAXCMS.basePath +
-                  HAXCMS.sitesDirectory + '/' +
-                  site.manifest.metadata.site.name + '/files/' + file
+              // ensure this is a file
+              if (
+                fs.lstatSync(siteFilePath + '/' + file).isFile()
+              ) {
+                // ensure this is a file and if we are searching for results then return only exact ones
+                if (!search || search == "" || file.indexOf(search) !== -1) {
+                  let fullUrl = '/files/' + file;
+                  // multiple sites then append the base url to site management area
+                  if (HAXCMS.operatingContext == 'multisite') {
+                    fullUrl = HAXCMS.basePath +
+                    HAXCMS.sitesDirectory + '/' +
+                    site.manifest.metadata.site.name + '/files/' + file
+                  }
+                  files.push({
+                    'path' : 'files/' + file,
+                    'fullUrl' : fullUrl,
+                    'url' : 'files/' + file,
+                    'mimetype' : mime.getType(siteFilePath + '/' + file),
+                    'name' : file
+                  });
                 }
-                files.push({
-                  'path' : 'files/' + file,
-                  'fullUrl' : fullUrl,
-                  'url' : 'files/' + file,
-                  'mimetype' : mime.getType(siteFilePath + '/' + file),
-                  'name' : file
-                });
+              } else {
+                  // @todo maybe step into directories?
               }
-            } else {
-                // @todo maybe step into directories?
             }
-          }
-        });
+          });
+        }
       }
     }
     res.send(files);
