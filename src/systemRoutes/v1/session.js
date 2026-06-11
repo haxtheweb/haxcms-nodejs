c961b0bc3f98b380d2786a92036434475eeca05a
btopro <bto108@psu.edu>
2026-06-11 19:13:21 -0400

    https://github.com/haxtheweb/issues/issues/2857

---
+++ src/systemRoutes/v1/session.js
@@ -0,0 +1,39 @@
+const loginRoute = require('../../routes/login.js');
+const logoutRoute = require('../../routes/logout.js');
+const refreshAccessTokenRoute = require('../../routes/refreshAccessToken.js');
+const connectionSettingsRoute = require('../../routes/connectionSettings.js');
+const connectionTestRoute = require('../../routes/connectionTest.js');
+const getUserDataRoute = require('../../routes/getUserData.js');
+
+async function login(req, res, next) {
+  return loginRoute(req, res, next);
+}
+
+async function logout(req, res, next) {
+  return logoutRoute(req, res, next);
+}
+
+async function refreshAccessToken(req, res, next) {
+  return refreshAccessTokenRoute(req, res, next);
+}
+
+async function connectionSettings(req, res, next) {
+  return connectionSettingsRoute(req, res, next);
+}
+
+async function connectionTest(req, res, next) {
+  return connectionTestRoute(req, res, next);
+}
+
+async function getUserData(req, res, next) {
+  return getUserDataRoute(req, res, next);
+}
+
+module.exports = {
+  login,
+  logout,
+  refreshAccessToken,
+  connectionSettings,
+  connectionTest,
+  getUserData,
+};
