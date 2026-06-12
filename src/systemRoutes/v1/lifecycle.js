95138f13ada8c284b3ecfa40c3aa78a1a4412cca
btopro <bto108@psu.edu>
2026-06-12 16:01:16 -0400

    https://github.com/haxtheweb/issues/issues/2857

--- src/systemRoutes/v1/lifecycle.js
+++ src/systemRoutes/v1/lifecycle.js
@@ -1,3 +1,6 @@
+const fs = require('fs-extra');
+const path = require('path');
+const { HAXCMS } = require('../../lib/HAXCMS.js');
 const listSitesRoute = require('../../routes/listSites.js');
 const createSiteRoute = require('../../routes/createSite.js');
 const cloneSiteRoute = require('../../routes/cloneSite.js');
@@ -30,6 +33,56 @@ function getSiteNameFromPayload(req) {
     return req.body.siteName.trim();
   }
   return '';
+}
+function buildSitePath(directory = '', siteName = '') {
+  return `${HAXCMS.HAXCMS_ROOT}${directory}/${siteName}`;
+}
+function isArchiveDestinationCollision(error = null) {
+  if (!error || typeof error !== 'object') {
+    return false;
+  }
+  return error.code === 'ENOTEMPTY' || error.code === 'EEXIST';
+}
+async function getNextArchivedSitePath(siteName = '') {
+  const archivedRoot = `${HAXCMS.HAXCMS_ROOT}${HAXCMS.archivedDirectory}`;
+  const basePath = `${archivedRoot}/${siteName}`;
+  if (!(await fs.pathExists(basePath))) {
+    return basePath;
+  }
+  let index = 1;
+  let candidatePath = `${basePath}-${index}`;
+  while (await fs.pathExists(candidatePath)) {
+    index += 1;
+    candidatePath = `${basePath}-${index}`;
+  }
+  return candidatePath;
+}
+async function archiveSiteWithCollisionResolution(req, res, error) {
+  if (!isArchiveDestinationCollision(error)) {
+    throw error;
+  }
+  const siteName = getSiteNameFromPayload(req);
+  if (siteName === '') {
+    throw error;
+  }
+  const archivedRoot = `${HAXCMS.HAXCMS_ROOT}${HAXCMS.archivedDirectory}`;
+  const sourcePath = buildSitePath(HAXCMS.sitesDirectory, siteName);
+  const destinationPath = await getNextArchivedSitePath(siteName);
+  await fs.ensureDir(archivedRoot);
+  await fs.rename(sourcePath, destinationPath);
+  const archivedName = path.basename(destinationPath);
+  const detail =
+    archivedName === siteName
+      ? 'Site archived'
+      : `Site archived as ${archivedName} because an archived copy already existed`;
+  return res.send({
+    status: 200,
+    data: {
+      name: siteName,
+      detail,
+      archivedName,
+    },
+  });
 }
 
 function applySiteNameFromParams(req) {
@@ -89,7 +142,12 @@ async function cloneSite(req, res, next) {
 
 async function archiveSite(req, res, next) {
   applySiteNameFromParams(req);
-  return archiveSiteRoute(req, res, next);
+  try {
+    return await archiveSiteRoute(req, res, next);
+  }
+  catch (error) {
+    return archiveSiteWithCollisionResolution(req, res, error);
+  }
 }
 
 async function downloadSite(req, res, next) {
